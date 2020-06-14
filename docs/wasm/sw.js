f0b2e315b638c9768d8c68f10bc21f8cbdd071d8
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-06-14 15:29:05 +0200

    :alembic: WASM HTTP experiment

--- docs/wasm/sw.js
+++ docs/wasm/sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@254bd5e2ea5b84b4ed2918f8edc7981fb24b06ad/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@b25c20e8f52c91754ad7294f7acc6b9cea149b14/sw.js')
