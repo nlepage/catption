443b3405579f9cd39eb2f49e3ae7615e41ab8cd7
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-01-22 15:31:40 +0100

    :alembic: Test new go-wasm-http-version

--- docs/wasm/sw.js
+++ docs/wasm/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@574bb8928c6a7a5c4feeaf7cee9530ea5e3c04b1/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@078ff3547ebe2abfbee1fd5af9ca5ad64be480c0/sw.js')
 
 addEventListener('install', (event) => {
   event.waitUntil(skipWaiting())
@@ -8,4 +8,4 @@ addEventListener('activate', event => {
   event.waitUntil(clients.claim())
 })
 
-registerWasmHTTPListener('catption.wasm', undefined, ['http'])
+registerWasmHTTPListener('catption.wasm', { args: ['http'] })
